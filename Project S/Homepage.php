ce15a71e13b22c06ca5805482a7c8b1131b111e0
Dofu <115920771+strdoffin@users.noreply.github.com>
2023-01-20 10:26:17 +0700

    Add files via upload

---
+++ Project S/Homepage.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="homepage.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.1/css/all.min.css" integrity="sha512-MV7K8+y+gLIBoVD59lQIYicR65iaqukzvf/nwasF0nqhPay5w/9lJmVM2hMDcnK1OnMGCdVK+iQrJ7lzPJQd1w==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <title>homepage</title>
+</head>
+<body>
+    <div class="nav">
+        <ul class="text-nav">
+            <li class="logo"><a href="">Iot Smart Farm</a></li>
+            <li class="right" onclick=""><i class="fa-sharp fa-solid fa-bars"></i></li>
+            <li class="right"><a href="">Control</a></li>
+            <li class="right"><a href="">Stat</a></li>
+            <li class="right"><a href="">Dashboard</a></li>
+        </ul>
+    </div>
+    <div class="mobile" id="show">
+        <ul>
+            <li><a href="">Control</a></li>
+            <li><a href="">Stat</a></li>
+            <li><a href="">Dashboard</a></li>
+        </ul>
+    </div>
+    <div style="width: 100%; height: 1000px;">
+        
+    </div>
+    <script>
+        
+    </script>
+</body>
+</html>
---
+++ Project S/mainacc.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="styleacc.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.1/css/all.min.css" integrity="sha512-MV7K8+y+gLIBoVD59lQIYicR65iaqukzvf/nwasF0nqhPay5w/9lJmVM2hMDcnK1OnMGCdVK+iQrJ7lzPJQd1w==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <title>Iot Smart Farm</title>
+</head>
+<body>
+    <div class="box" >
+        <div class="show" style="">
+            <div class="text">
+                <h1 class="font">Iot Smart Farm<i class="fa-solid fa-leaf"></i></h1>
+            </div>
+            <a href="login.php">
+                <div class="login">
+                    <h1 class="pipi">เข้าสู่ระบบ</h1>
+                </div>
+            </a>
+            <a href="register.php">
+                <div class="regis">
+                    <h1 class="pipi">สมัครสมาชิก</h1>
+                </div>
+            </a>
+            
+        </div>
+    </div>
+    <script>
+        
+    </script>
+</body>
+</html>
